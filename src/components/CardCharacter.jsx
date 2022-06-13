981a790e9b1db3ff3faa97691d434beb71e78ae4
Luis N. Martinez <boue123@gmail.com>
2022-06-13 01:53:01 -0500

    Last changesII

--- src/components/CardCharacter.jsx
+++ src/components/CardCharacter.jsx
@@ -11,14 +11,15 @@ const character = useCharacterApi(resident)
 
 
   return (
-    <article className='card-character'>
-        <img src={character?.image} class="card-img-top" alt='' />
-        <h3 class="list-group-item" >Name: {character?.name}</h3>
-        <p class="card-text"><b>Status: </b> {character?.status}</p>
-        <p class="card-text"><b>Origin: </b> {character?.origin.name}</p>
-        <p class="card-text"><b>Espisodes: </b> {character?.episode.length}</p>
-
-    </article>
+    <div className='card-character'>
+      <article>
+          <img src={character?.image} class="card-img-top" alt='' />
+          <h3 class="list-group-item" >Name: {character?.name}</h3>
+          <p class="card-text"><b>Status: </b> {character?.status}</p>
+          <p class="card-text"><b>Origin: </b> {character?.origin.name}</p>
+          <p class="card-text"><b>Espisodes: </b> {character?.episode.length}</p>
+      </article>
+    </div>
   )
 }
 
